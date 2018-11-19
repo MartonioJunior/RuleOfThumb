d82e63caab90f17398a6905599f7226162ae3d9f
Martônio Júnior <martoniojunior@MacBook-Pro-93.local>
2018-11-19 10:04:15 -0300

    Created git repository

---
+++ RuleOfThumb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RuleOfThumb
+//
+//  Created by Martônio Júnior on 19/11/18.
+//  Copyright © 2018 So Many Deadlines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
