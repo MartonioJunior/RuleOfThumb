084ffd4c5d701a037651e55c81265182f20d9bf0
Matheus Oliveira Costa <matheusoliveiracos@gmail.com>
2018-11-23 11:14:23 -0300

    Modelos cloudkit de regras e casa
    Nesse commit foi adicionado os modelos para salvar os dados no
    cloudkit. Já pensando na integração com o coredata fiz algumas classes e
    protocolos mais gerais, por isso o commit é meio grande. Mas está
    completamente funcional.

---
+++ RuleOfThumb/Shared/Entities/House.swift
@@ -0,0 +1,48 @@
+//
+//  House.swift
+//  RuleOfThumb
+//
+//  Created by Matheus Costa on 21/11/18.
+//  Copyright © 2018 So Many Deadlines. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class House: CKManagedObject {
+    var recordType: String
+    var recordID: Data?
+    var recordName: String?
+    
+    var name: String
+    var openKey: String
+    
+    init(name: String) {
+        self.name = name
+        self.openKey = "ABC123"
+        self.recordType = "Houses"
+        self.recordName = self.recordType + "." + UUID().uuidString
+        
+        let tempId = CKRecord.ID(recordName: self.recordName!)
+        self.recordID = self.ckRecordIDToData(recordID: tempId)
+    }
+    
+    required init(from record: CKRecord) {
+        self.name = record.value(forKey: "name") as! String
+        self.openKey = record.value(forKey: "openKey") as! String
+        self.recordName = record.value(forKey: "recordName") as? String
+        self.recordType = record.recordType
+        
+        let recordID = record.recordID
+        self.recordID = self.ckRecordIDToData(recordID: recordID)
+    }
+    
+    func toCKRecord() -> CKRecord {
+        let record = CKRecord(recordType: self.recordType, recordID: self.ckRecordId())
+        
+        record["name"] = self.name as CKRecordValue
+        record["openKey"] = self.openKey as CKRecordValue
+        
+        return record
+    }
+}
---
+++ RuleOfThumb/Shared/SyncManager.swift
@@ -0,0 +1,9 @@
+//
+//  SyncManager.swift
+//  RuleOfThumb
+//
+//  Created by Matheus Costa on 21/11/18.
+//  Copyright © 2018 So Many Deadlines. All rights reserved.
+//
+
+import Foundation
