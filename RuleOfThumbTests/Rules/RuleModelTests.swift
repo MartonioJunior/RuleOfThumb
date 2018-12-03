863b3b49236ccb3bc6a8ebdadfb8576b048d3ff9
Martônio Júnior <martoniojunior@10-45-40-42.0101.ifce.br.iosda.org>
2018-12-03 08:56:30 -0300

    Started Rule Tests and cleared unused files

---
+++ RuleOfThumbTests/Rules/RuleModelTests.swift
@@ -0,0 +1,19 @@
+//
+//  RuleModelTests.swift
+//  RuleOfThumbTests
+//
+//  Created by Martônio Júnior on 03/12/18.
+//  Copyright © 2018 So Many Deadlines. All rights reserved.
+//
+
+import XCTest
+
+class RuleModelTests: XCTestCase {
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+}
