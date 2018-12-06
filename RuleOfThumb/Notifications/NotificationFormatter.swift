37d728ce25f5ff3d42516dbcd23e2adc6ff0867c
Martônio Júnior <martoniojunior@10-45-40-42.0101.ifce.br.iosda.org>
2018-12-06 09:03:09 -0300

    Started push notifications formatting

---
+++ RuleOfThumb/Notifications/NotificationFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationFormatter.swift
+//  RuleOfThumb
+//
+//  Created by Martônio Júnior on 06/12/18.
+//  Copyright © 2018 So Many Deadlines. All rights reserved.
+//
+
+import UIKit
+
+class NotificationFormatter: NSObject {
+
+}
